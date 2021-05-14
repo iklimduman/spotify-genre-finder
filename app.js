7de65336a16de6724464c86aba927aa751d2f7c6
iklimduman <iklimdumann@gmail.com>
2021-05-14 23:02:03 +0300

    handle post request

--- app.js
+++ app.js
@@ -4,9 +4,11 @@ const urlencoded = require("body-parser/lib/types/urlencoded");
 
 const app = express();
 
-app.use(express.urlencoded({
-    extended: true
-}));
+app.use(bodyParser.urlencoded(
+    {
+        extended : true
+    }
+));
 
 app.use(express.static(__dirname + '/public'));
 
@@ -14,7 +16,16 @@ app.get("/",function(req,res){
     res.sendFile(__dirname+"/index.html");
 });
 
+app.post("/",function(req,res){
+    res.send("Thanks for posting that");
+    console.log(req.body);
+})
+
 
 app.listen(3000,function(){
     console.log("app is running on port 3000");
 });
+
+// spotify api key
+
+// last fm api key
